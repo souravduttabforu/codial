8fe5b8a35e031ee6d87a5836eff6d897eac1d0ca
Sourav Dutta <souravduttabforu@gmail.com>
2023-02-03 01:33:22 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`error in starting the server : ${err}`);
+    }
+    console.log(`Server is running on : ${port}`);
+});
