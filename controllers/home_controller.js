fd9e7024c81797452c610320e4232aae6c86b6c6
Sourav Dutta <souravduttabforu@gmail.com>
2023-02-21 10:35:17 +0530

    fourteenth commit: Showing Comment

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,17 +1,25 @@
-const Post = require('../models/post')
-module.exports.home = function(req,res){
-    // console.log(req.cookies);
-//     Post.find({},function(err,posts){
-//         return res.render('home',{
-//             title : 'Codial | Home',
-//             posts: posts
-//     })   
-//   })
+const Post = require("../models/post");
+module.exports.home = function (req, res) {
+  // console.log(req.cookies);
+  //     Post.find({},function(err,posts){
+  //         return res.render('home',{
+  //             title : 'Codial | Home',
+  //             posts: posts
+  //     })
+  //   })
   //populate the user of each post
-  Post.find({}).populate('user').exec(function(err,posts){
-    return res.render('home',{
-        title: 'Codial| Home',
-        posts: posts
+  Post.find({})
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
     })
-  })
-}
+    .exec(function (err, posts) {
+      return res.render("home", {
+        title: "Codial| Home",
+        posts: posts,
+      });
+    });
+};
